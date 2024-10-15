92fd96c2b01efd40f26332d8512ed1955b47656c
Lucas Yuji Furukawa <165024160+yujifkw@users.noreply.github.com>
2024-10-15 14:31:17 -0300

    Create 1020 C99.c

---
+++ beecrowd/1020 C99.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main(){
+    int dia, mes, ano;
+    scanf("%d", &dia);
+    
+    ano = dia / 365;
+    dia = dia % 365;
+    mes = dia / 30;
+    dia = dia % 30;
+    
+    printf("%d ano(s)\n", ano);
+    printf("%d mes(es)\n", mes);
+    printf("%d dia(s)\n", dia);
+ 
+    return 0;
+}
