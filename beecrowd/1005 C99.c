a3de8d92eba04aee11900e1de0d14c9c8a3bbc43
Lucas Yuji Furukawa <165024160+yujifkw@users.noreply.github.com>
2024-11-05 17:41:48 -0300

    Create 1005 C99.c

---
+++ beecrowd/1005 C99.c
@@ -0,0 +1,17 @@
+/*
+ * nome: 1005 C99.c
+ * descrição: Average 1
+ * data: -
+ */
+
+#include <stdio.h>
+ 
+int main() {
+    
+    double a, b, m;
+    scanf("%lf %lf", &a, &b);
+    m=((a*3.5)+(b*7.5))/11;
+    printf("MEDIA = %0.5lf\n", m);
+ 
+    return 0;
+}
