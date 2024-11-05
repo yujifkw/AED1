cdf3a57b03154cab2df7e3d1960dfad85d854396
Lucas Yuji Furukawa <165024160+yujifkw@users.noreply.github.com>
2024-11-05 17:43:42 -0300

    Create 1007 C99.c

---
+++ beecrowd/1007 C99.c
@@ -0,0 +1,19 @@
+/*
+ * nome: 1007 C99.c
+ * descrição: Difference
+ * data: -
+ */
+
+#include <stdio.h>
+ 
+int main() {
+ 
+   int a, b, c, d, D;
+   scanf("%i%i%i%i", &a, &b, &c, &d);
+   
+   D = (a*b)-(c*d);
+   
+   printf("DIFERENCA = %i\n", D);
+ 
+    return 0;
+}
