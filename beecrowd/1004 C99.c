6de32aa5c20ca77135d56d160944d41899519bb4
Lucas Yuji Furukawa <165024160+yujifkw@users.noreply.github.com>
2024-11-05 17:40:20 -0300

    Create 1004 C99.c

---
+++ beecrowd/1004 C99.c
@@ -0,0 +1,19 @@
+/*
+ * nome: 1004 C99.c
+ * descrição: Simple Product
+ * data: -
+ */
+
+#include <stdio.h>
+ 
+int main() {
+ 
+    int x, y, p;
+    scanf("%i%i", &x, &y);
+    
+    p = x * y;
+    
+    printf("PROD = %i\n", p);
+ 
+    return 0;
+}
