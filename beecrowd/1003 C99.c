5508e9000a6f703d0974e16aa84da062fc45f5b2
Lucas Yuji Furukawa <165024160+yujifkw@users.noreply.github.com>
2024-11-05 17:39:15 -0300

    Create 1003 C99.c

---
+++ beecrowd/1003 C99.c
@@ -0,0 +1,16 @@
+/*
+ * nome: 1001 C99.c
+ * descrição: Simple Sum
+ * data: -
+ */
+
+#include <stdio.h>
+ 
+int main() {
+    int x, y, s;
+    scanf("%i%i", &x, &y);
+    s=x+y;
+    printf("SOMA = %i\n", s);
+    
+    return 0;
+}
