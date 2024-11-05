a514c6488280e385038131fc96b8109cbde5658c
Lucas Yuji Furukawa <165024160+yujifkw@users.noreply.github.com>
2024-11-05 17:52:09 -0300

    Create 1016 C99.c

---
+++ beecrowd/1016 C99.c
@@ -0,0 +1,17 @@
+/*
+ * nome: 1016 C99.c
+ * descrição: Distance
+ * data: -
+ */
+
+#include <stdio.h>
+ 
+int main() {
+    int vy, km, t;
+    scanf("%i", &km);
+    
+    t = km*2;
+    printf("%i minutos\n", t);
+ 
+    return 0;
+}
