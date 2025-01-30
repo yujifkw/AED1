ff37a2ba18f1438402f56e8924e41545c36d2f94
Lucas Yuji Furukawa <165024160+yujifkw@users.noreply.github.com>
2025-01-29 21:41:06 -0300

    Create 1043 C99.c

---
+++ beecrowd/1043 C99.c
@@ -0,0 +1,17 @@
+/*
+ * nome: 1043 C99.c
+ * descrição: Triangle
+ * data: 29/01
+*/
+
+#include <stdio.h>
+
+int main() {
+    float A, B, C;
+    scanf("%f%f%f", &A, &B, &C);
+    
+    if (A+B>C && A+C>B && B+C>A) printf("Perimetro = %.1f\n", A+B+C);
+    else printf("Area = %.1f\n", (A+B)*C/2);
+
+    return 0;
+}
